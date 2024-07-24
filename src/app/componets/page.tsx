6397037293efec1d970caead324763bde1a61103
Keonbangjin <ochilovasliddin27@gmail.com>
2024-07-24 21:46:46 +0500

    a

--- src/app/componets/page.tsx
+++ src/app/componets/page.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 import pointer from '../assets/pointer.svg';
 import restagelImg from '../assets/Rectangle layout-img.svg';
 
+// Define the type for props
 interface PageProps {
     pageName: string;
 }
 
+// Define the component with the correct type
 const Page: React.FC<PageProps> = ({ pageName }) => {
     return (
         <div className="relative flex items-center justify-center">
-            <Image className="z-0" src={restagelImg} alt="res" />
+            <Image className="z-0" src={restagelImg} alt="restagel" />
             <div className="absolute top-10 w-[500px] h-[100px] flex flex-col items-center justify-center mt-[70px]">
                 <h1 className="text-5xl font-medium mb-5">{pageName}</h1>
-                <h2 className="flex gap-5 text-base font-medium">Home <Image src={pointer} alt="res" /> {pageName}</h2>
+                <h2 className="flex gap-5 text-base font-medium">Home <Image src={pointer} alt="pointer" /> {pageName}</h2>
             </div>
         </div>
     );
